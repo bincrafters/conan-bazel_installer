6cdf875a4750f658583bd664fd57f36cc99640d2
Jerry W <jerrywiltse@gmail.com>
2017-12-05 16:27:28 -0500

    try fix build_requires build.py

--- build.py
+++ build.py
@@ -57,5 +57,5 @@ if __name__ == "__main__":
         settings={"os" : get_os(), "arch" : "x86_64"}, 
         options={}, 
         env_vars={}, 
-        build_requires={"*" : ["msys_installer/20161025@bincrafters/stable"]}) 
+        build_requires={"*" : ["msys2_installer/20161025@bincrafters/stable"]}) 
     builder.run()
