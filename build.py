4df440d22686d63d4856b882c6188f3a25f9f340
SSE4 <tomskside@gmail.com>
2019-07-10 17:36:32 +0200

    - actually build something
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.py
+++ build.py
@@ -2,10 +2,12 @@
 # -*- coding: utf-8 -*-
 
 
-from bincrafters import build_template_installer
+from bincrafters import build_template_installer, build_shared
+import os
 
 if __name__ == "__main__":
 
+    arch = os.environ["ARCH"]
     builder = build_template_installer.get_builder()
-
+    builder.add({"os" : build_shared.get_os(), "arch_build" : arch, "arch": arch}, {}, {}, {})
     builder.run()
